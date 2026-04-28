6f45c430232044868427f2b44ff69ce8878f4a51
Soc Basillote <dicecepe@gmail.com>
2026-04-28 12:19:52 +0800

    basic modal

--- verybasic/dropdown/script.js
+++ verybasic/dropdown/script.js
@@ -3,17 +3,29 @@ const dropdown = document.getElementById("dropdown");
 // Toggle dropdown
 document.addEventListener("click", function (e) {
     const button = e.target.closest("[data-dropdown-button]");
-
+ //   console.log(button);
     // If clicking the button -> toggle
     if (button) {
-        const parent = button.closest(".dropdown");
-        parent.classList.toggle("active");
+        const currentDropdown = button.closest(".dropdown");
+
+        //Close sibling dropdowns (save level)
+        const parentMenu = currentDropdown.parentElement;
+        const siblingDropdowns = parentMenu.querySelectorAll(":scope > .dropdown");
+
+        siblingDropdowns.forEach(drop => {
+            if (drop !== currentDropdown) {
+                drop.classList.remove("active");
+            }
+        })
+
+        // Toggle current
+        currentDropdown.classList.toggle("active");
         return;
     }
 
     // If clicking outside -> close ALL dropdowns
     const allDropdown = document.querySelectorAll(".dropdown");
-
+    console.log(allDropdown)
     allDropdown.forEach(drop => {
         if (!drop.contains(e.target)){
             drop.classList.remove("active");
---
+++ verybasic/modal/script.js
@@ -0,0 +1,27 @@
+// OPEN MODAL
+document.addEventListener("click", function (e) { 
+    const openBtn = e.target.closest("[data-modal-open]");
+    if (openBtn) {
+        const selector = openBtn.dataset.modalOpen;
+        const modal = document.querySelector(selector);
+        if (modal) modal.classList.add("active");
+        return;
+    }
+
+    // CLOSE MODAL (button or overlay)
+    const closeBtn = e.target.closest("[data-modal-close]");
+    if (closeBtn) {
+        const modal = closeBtn.closest(".modal");
+        if (modal) modal.classList.remove("active");
+        return;
+    }
+});
+
+// ESC KEY CLOSE
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        document.querySelectorAll(".modal.active").forEach(modal => {
+            modal.classList.remove("active");
+        });
+    }
+})
