b67e0a8a18672260fb1c923c8b4ef1bd0f970725
Soc Basillote <dicecepe@gmail.com>
2026-05-16 13:48:55 +0800

    practice

---
+++ _ablocks/phase1.js
@@ -0,0 +1,97 @@
+/* //1. Objects
+
+const user = {
+    name: "Soc",
+    age: 29,
+    isAdmin: false
+};
+
+user.age = 30;
+user.address = 'Cebu City'
+console.log(user)
+ */
+
+/* // 2. Array
+const products = [
+    { id: 1, name: 'Laptop'},
+    { id: 2, name: 'Phone'},
+];
+
+products.push({
+    id: 3, name: "Car"
+})
+
+const product = products.find(p => p.id === 2);
+ */
+
+/* // 3. Immutability
+// Why immutability matters? frameworks detect changes easier.
+
+
+const user = [ 
+    { name: "Soc", age: 29, isAdmin: false },
+    { name: "Rates", age: 30, isAdmin: false },
+];
+
+const updateUser = user.map(u => (
+     u.name === 'Soc' ? {...u, name: 'anthony'}: user
+));
+
+const removeUser = user.filter(u => u.name !== 'Soc');
+
+const toggle = user.map(u => (
+    u.name === 'Rates' ? {...u, isAdmin: true} : u
+))
+ */
+
+/* // 4. map / filter / reduce
+// map - transforms data
+// filter - keep matching items
+// reduce - accumulate values
+
+
+const user = [ 
+    { id: 1, title: 'JS maxxing', active: false, price: 10},
+    { id: 2, title: 'immutability', active: false, price: 15 },
+    { id: 3, title: 'user3', active: true, price: 20 },
+    { id: 4, title: 'username', active: false, price: 5 },
+    { id: 5, title: 'userAgent', active: true, price: 25 },
+];
+
+
+const upperCase = user.map(u => u.title.toUpperCase());
+const getActive = user.filter(u => u.active === true);
+const totalPrice = user.reduce((s, t) => {
+    return s + t.price
+}, 10)
+
+
+console.log(totalPrice); */
+
+/* // 5. IDs
+// Ids uniquely identify items
+
+const todos = [];
+
+const idDate = crypto.randomUUID();
+
+function createTodo(title){
+    return {
+        id: idDate,
+        title: title
+    };
+}
+
+todos.push(createTodo('helloTitle'));
+todos.push(createTodo('secondTitle'));
+
+console.log(todos); */
+
+// 6. Event Listener
+const app = document.getElementById('app');
+
+app.addEventListener('click', () => {
+    console.log('click')
+})
+
+
